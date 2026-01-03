437833cb65b2c5d6b59a15ed7d4c8e3db4736ed5
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2026-01-03 14:49:11 +0000

    da

---
+++ day1.py
@@ -0,0 +1,16 @@
+# num = float(input())
+
+# integral_num = int(num)
+
+# right_digit = integral_num % 20
+
+# print(right_digit)
+
+# 2. Write a program to read two numbers from the user and compute – 1st number to the power of the 2nd number.
+
+a = float(input())
+b = float(input())
+
+result = a ** b
+
+print(f"{result:.2f}")
