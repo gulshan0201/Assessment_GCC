22c5feb6f98331088fbbc20fb5746cb07ee09e82
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2026-01-14 05:29:38 +0000

    ss

--- day2.c
+++ day2.c
@@ -1,3 +1,21 @@
-int a = 10;
-int b = 20;
-int c = a ^ b;
+
+// Write a C Program multiplies given number by 4 using bitwise operators.
+#include <stdio.h>
+
+int main() {
+    int number, tempnum;
+
+    // Read input
+    scanf("%d", &number);
+
+    // Assign number to tempnum
+    tempnum = number;
+
+    // Multiply by 4 using left shift operator
+    tempnum = tempnum << 2;
+
+    // Print result
+    printf("%d", tempnum);
+
+    return 0;
+}
